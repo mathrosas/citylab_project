5b672688aeaa243693dac84e6d13419e999fd63f
mathrosas <mathiasrosas@gmail.com>
2025-03-27 12:39:39 +0000

    Added rviz visualization

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,10 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('robot_patrol')
+    rviz_config = os.path.join(pkg_share, 'config', 'robot_patrol_config.rviz')
+
+    patrol_node = Node(
+        package='robot_patrol',
+        executable='patrol_node',
+        output='screen',
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config]
+    )
+
     return LaunchDescription([
-        Node(
-            package='robot_patrol',
-            executable='patrol_node',
-            output='screen'),
+        patrol_node,
+        rviz_node
     ])
